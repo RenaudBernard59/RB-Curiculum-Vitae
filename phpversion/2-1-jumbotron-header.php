9bec62c4734c3cc66f245e1d79d7fb1ea2f06d1c
Renaud BERNARD <renaud.bernard@p59production.com>
2017-10-04 00:57:18 +0200

    Exploding the index file in different php files - First step

---
+++ phpversion/2-1-jumbotron-header.php
@@ -0,0 +1,66 @@
+ <!-- Main jumbotron for a primary marketing message or call to action -->
+ <header class="jumbotron">
+  <section class="container">
+   <section>
+     <h1>Renaud BERNARD le Curiculum Vitae</h1>
+     <p>Ancien étudiant en géologie. Passionné d'Audiovisuel. Etudiant en Développement Web à SIMPLON Roubaix</p>
+     <p><a class="btn btn-primary" href="#mesinfos" data-toggle="collapse" aria-expanded="false" aria-controls="mesinfos" role="button">Informations supplémentaires &raquo;</a></p>
+   </section>
+   <!-- ########### -->
+   <section id="mesinfos"  class="collapse">
+    <h2>Présentation</h2>
+    <article itemscope itemtype="http://schema.org/Person" class="vcard">
+      <h3>Coordonnées</h3>
+      <div class="col-md-4">
+        <strong>Dénomination</strong>
+        <p>
+          Nom : <span itemprop="name" class="fn"><span itemprop="givenName" class="given-name">Renaud</span> <span itemprop="familyName" class="family-name">BERNARD</span></span><br/>
+          Date de naissance: <time datetime="1992-01-31" class="bday"><span itemprop="birthDate">31/01/1992</span></time><br/>
+          Statut : <span itemprop="jobTitle" class="category">Etudiant</span>
+        </p>
+      </div>
+      <address>
+        <div class="col-md-4">
+          <strong>Coordonnées</strong>
+          <p>
+            Téléphone : <span itemprop="telephone" class="tel">+33 (0)6 31 63 61 82<br/></span>
+            Mail : <a href="mailto:renaud.bernard@p59production.com" itemprop="email" class="email">renaud.bernard@p59production.com</a>
+          </p>
+        </div>
+        <div class="col-md-4">
+          <strong>Adresse postale</strong>
+
+          <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress" class="adr">
+            <p>
+              Rue : <span itemprop="streetAddress" class="street-address">1158, route de Méteren</span><br/>
+              Code postal : <span itemprop="postalCode" class="postal-code">59270</span><br/>
+              Ville : <span itemprop="addressLocality"  class="locality">Bailleul</span><br/>
+              Région : <span itemprop="addressRegion" class="region">Nord</span><br/>
+              Pays : <span itemprop="addressCountry" class="country-name">France</span>
+            </p></div>
+
+          </div>
+        </address>
+      </article><!-- END vcard -->
+      <!-- ########### -->
+      <article class="supplements">
+        <h3>Suppléments</h3>
+        <strong>Suppléments</strong>
+        <p><abbr title="Reconnaissance Travailleurs Handicaper">RQTH</abbr></p>
+      </article>
+      <!-- ########### -->
+      <article class="langues">
+        <h3>Langues</h3>
+        <ul>
+          <li>Français : Langue natale. Niveau C2 oral, B1A2 grammaire ;</li>
+          <li>Anglais : Niveau A2 oral et grammaire, Bon niveau en compréhension écrite technique ;</li>
+          <li>Espagnol : Quelques notions.</li>
+        </ul>
+      </article>
+
+      <!-- ########################################################### -->
+    </section> <!--END mesinfos -->
+    <!-- ########### -->
+    <img src="img/renaud-bernard0001.jpg" alt="Renaud BERNARD" class="col-md-2" />
+  </section><!--END container -->
+  </header><!--END jumbotron -->
---
+++ phpversion/2-2-competences-generales.php
@@ -0,0 +1,261 @@
+<section>
+<h2>Compétences générales</h2>
+<p>Etudes générales et certifications en tout genre</p>
+<p><a class="btn btn-danger" href="#section1" data-toggle="collapse" aria-expanded="false" aria-controls="section1" role="button">J'en veux plus !! &raquo;</a></p>
+</section>
+<section id="section1"  class="collapse">
+
+<article class="etudes">
+    <h4>Études diplômantes</h4>
+    <ul class="vcalendar">
+        <li class="education vevent"><span class="dtend" title="2010-06-30"><time datetime="2010-06-30">30/06/2010</time></span> : Baccalauréat Scientifique spécialité SVT, mention AB<a class="url fn org" href="http://www.saint-jude.fr/">Lycée Saint Jude</a>,
+        <span class="adr"><span class="street-address">18, Rue Lamartine</span>  <span class="postal-code">59280</span> <span class="locality">Armentières</span>,  <span class="region" title="Nord">Nord</span>, <span class="country-name">France</span></span> ;</li>
+        <li class="education vevent"><span class="dtend" title="2007-06-30"><time datetime="2007-06-30">30/06/2007</time></span> : Brevet des Collèges, mention AB.
+        <a class="url fn org" href="https://www.ic-bailleul.com/">Colège Immaculé Conception</a>,
+        <span class="adr"><span class="street-address">22, Rue du collège</span>  <span class="postal-code">59270</span> <span class="locality">Bailleul</span>,  <span class="region" title="Nord">Nord</span>, <span class="country-name">France</span></span>.</li>
+    </ul>
+</article><!-- END etudes -->   
+<!-- ########### -->
+<article class="logiciels">
+<h3>Compétences informatiques et logiciels</h3>
+<ul>
+
+<li class="listelogiciels">Systèmes d’exploitations :
+<ul>
+<li>Microsoft Windows 95, 98, 2000/Millenium, XP, Vista, 7, 8.1, 10 ;</li>
+<li>GNU/Linux Gnome, Ubuntu, Kubuntu, Xubuntu ;</li>
+<li>Android ;</li>
+<li>Microsoft Windows Mobile 7 ;</li>
+<li>iOS.</li>
+</ul>
+</li>
+<li class="listelogiciels">Bureautiques général, mind mapping, GdP :a
+<ul>
+<li class="listelogiciels">Microsoft :
+<ul>
+<li>Word, Excel, PowerPoint, OneNote ;</li>
+<li>Outlook, Outlook.com ;</li>
+<li>Visio, Project.</li>
+</ul>
+</li>
+<li class="listelogiciels">Open Source :
+<ul>
+<li>OpenOffice & Libre Office Writer, Calc, Impress, Draw, Math ;</li>
+<li>Mozilla Thunderbird, SeaMonkey, KMail ;</li>
+<li>VUE, CmapTools, XMind.</li>
+</ul>
+</li>
+<li class="listelogiciels">Google :
+<ul>
+<li>Docs, Sheets, Slides KeepNote ;</li>
+<li>Gmail ;</li>
+<li>Drawings, MyMaps, PaperPile.</li>
+</ul>
+</li>
+</ul>
+</li>
+<li class="listelogiciels">Navigateurs :
+<ul>
+<li>Microsoft Internet Explorer, Edge ;</li>
+<li>Netscape, Flock ;</li>
+<li>Mozilla Firefox, Firefox beta, Firefox developer edition, SeaMonkey, Tor ;</li>
+<li>Google Chrome, Chrome Canary, Chromium ;</li>
+<li>Maxthon Cloud Browser, Yandex Browser, UC Browser ;</li>
+<li>Opera, Opera beta, Opera developer.</li>
+</ul>
+</li>
+<li class="listelogiciels">Réseaux Sociaux :
+<ul>
+<li class="listelogiciels">Généraux :
+<ul>
+<li>Facebook ;</li>
+<li>Twitter ;</li>
+<li>Google+.</li>
+</ul>
+</li>
+<li class="listelogiciels">Photographie :
+<ul>
+<li>Instagram ;</li>
+<li>Flickr ;</li>
+<li>500px.</li>
+</ul>
+</li>
+<li class="listelogiciels">Professionnels :
+<ul>
+<li>LinkedIn ;</li>
+<li>SlidesShare ;</li>
+<li>Viadeo.</li>
+</ul>
+</li>
+<li class="listelogiciels">Questions/Réponses :
+<ul>
+<li>Quora ;</li>
+<li>Open2Leads ;</li>
+<li>Ask.fm.</li>
+</ul>
+</li>
+<li class="listelogiciels">Tableaux intéractifs et links sharing :
+<ul>
+<li>Pinterest ;</li>
+<li>Flipboard ;</li>
+<li>Delicious ;</li>
+<li>StumbleUpon ;</li>
+<li>Reddit.</li>
+</ul>
+</li>
+<li class="listelogiciels">Vidéos :
+<ul>
+<li>Youtube ;</li>
+<li>Dailymotion ;</li>
+<li>Vimeo ;</li>
+<li>Twitch ;</li>
+<li>Vine.</li>
+</ul>
+</li>
+<li class="listelogiciels">Musique et Audio :
+<ul>
+<li>SoundCloud ;</li>
+<li>MixCloud ;</li>
+<li>iTunes ;</li>
+<li>Deezer ;</li>
+<li>Last.fm.</li>
+</ul>
+</li>
+<li class="listelogiciels">Bloging et MicroBloging :
+<ul>
+<li>Wordpress ;</li>
+<li>Tumblr ;</li>
+<li>Skyrock Blog ;</li>
+<li>Myspace ;</li>
+<li>Blogger.</li>
+</ul>
+</li>
+<li class="listelogiciels">Spécialisés :
+<ul>
+<li>MyGameList.fr ;</li>
+<li>Betaseries ;</li>
+<li>GitHub ;</li>
+<li>ResearchGate ;</li>
+<li>SteamCommunity ;</li>
+<li>XboxLive ;</li>
+<li>Club Ubi ;</li>
+<li>EA Origin ;</li>
+<li>PlayStation Network ;</li>
+<li>Account Nintendo ;</li>
+<li>Hatebook.</li>
+</ul>
+</li>
+</ul>
+</li>
+<li class="listelogiciels">Gestionnaires de réseaux sociaux et tableaux de travail :
+<ul>
+<li>Scoop.it ;</li>
+<li>HootSuite ;</li>
+<li>IFTTT.</li>
+</ul>
+</li>
+<li class="listelogiciels">Clouds :
+<ul>
+<li>Microsoft OneDrive ;</li>
+<li>Google Drive ;
+<li>Amazn Drive ;</li>
+<li>Adobe Creative Cloud ;</li>
+<li>DropBox ;</li>
+<li>OVH Hubic ;</li>
+<li>ASUS Web Storage ;</li>
+<li>Orange Cloud.</li>
+</ul>
+</li>
+</ul>
+</article><!-- END logiciciels -->
+<!-- ########### -->
+
+
+<!-- ########### -->
+
+
+<!-- ########### -->
+
+
+
+</section>
+<!-- ########################################################### -->
+
+
+                    
+
+                        <article class="certifications-mooc">
+                            <h4>Certifications de suivi de MOOCS</h4>
+
+                            <ul class="vcalendar">
+                                <li class="education vevent"><span class="dtstart" title="2015-09-01"><time datetime="2015-09-01">01/09/2015</time></span> - <span class="dtend" title="2015/11/30"><time datetime="2015-11-30">31/11/2015</time></span> :
+                                    MOOC Gestion de Projet. <a class="url fn org" href="http://www.ec-lille.fr/">École Centrale de Lille</a>,
+                                    <span class="adr">Cité Scientifique. <span class="postal-code">59650</span> <span class="locality">Villeneuve d’Ascq</span>,  <span class="region" title="Nord">Nord</span>, <span class="country-name">France</span></span>.</li>
+                                </ul>
+
+                            </article>
+                            <article class="attestations-mooc">
+                                <h4>Attestations de suivi de MOOCS</h4>
+
+                                <ul class="vcalendar">
+                                    <li class="education vevent"><span class="dtend" title="2015-12-31"><time datetime="2015-12-31">31/12/2015</time></span> - CreativeBox - Parcours créatif. ESSCA Business School, Ecole de management ;</li>
+                                    <li class="education vevent"><span class="dtend" title="2015-11-30"><time datetime="2015-11-30">30/11/2015</time></span> - Comment financer son projet solidaire. OpenClassrooms ;</li>
+                                </ul>
+                            </article>
+
+                            <article class="experience-pro">
+                                <h2>Expérience professionnelle</h2>
+                                <article class="stages">
+                                    <h3>Stages</h3>
+                                    <article class="stages-scolaires">
+                                        <h4>Stages scolaires</h4>
+                                        <ul>
+                                            <li><time datetime="2008-06-16">16/06/2008</time> - <time datetime="2008-06-19">19/06/2008</time> - Stage en entreprise : SARL Marissael Francis. Responsable Francis Marissael. 19 rue du Comte d’Artois, 59285 Arneke ;</li>
+                                            <li><time datetime="2006-11-13">13/11/2006</time> - <time datetime="2006-11-14">14/11/2006</time> - Stage en entreprise : SNE IEM. Responsable Dominique Bernard SNE IEM, 21 rue Duhamel Liard, 59660 Merville, Nord, France.</li>
+                                            <li><time datetime="2006-03-25">25/03/2006</time> - <time datetime="2006-03-27">27/03/2006</time> - Stage en entreprise : GPAC Lille BNP Paribas. Responsable Dominique Moret. BNP Paribas, 101 rue de l'Hôpital Militaire, 59023 Lille, Nord, France ;</li>
+                                        </ul>
+                                    </article>
+                                </article>
+                            </article>
+                            <article class="associations">
+                             <article class="assocs-divers">
+                                <h3>Divers</h3>
+                                <ul>
+                                    <li><time datetime="2017-01-01">01/01/2017"</time> - <time datetime="2017-12-31">31/12/2017</time> - Ass des As. 1, Boulevard du Professeur Jules Leclercq  (2ème étage), 59000 Lille, Nord, France.
+                                        <ul>
+                                            <li>Présentation :
+                                                <ul>
+                                                    <li>Association Régionale des Asperger et autistes de haut niveau.</li>
+                                                </ul>
+                                            </li>
+                                            <li>Rôles :
+                                                <ul>
+                                                    <li>Membre.</li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li><time datetime="2005-01-01">01/01/2005</time> - <time datetime="2009-12-31">31/12/2009</time> : Association Jeunes en Fêtes Bailleul (JeF Bailleul). Collège Immaculé Conception, 22, rue du Collège , 59270 Bailleul, Nord, France.
+                                        <ul>
+                                            <li>Présentation :
+                                                <ul>
+                                                    <li>Organisation de soirées/Veillées à thèmes pour jeunes collégiens du Collège Immaculé Conception.
+                                                    </ul>
+                                                </li>
+                                                <li>Rôles :
+                                                    <ul>
+                                                        <li>Organisation et animation des Veillées ;</li>
+                                                        <li>Pratique de la Photo et de la vidéo ;</li>
+                                                        <li>Musicien.</li>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </article>
+                            </article>
+
+
+                            <!-- ########################################################### -->
+                        
+                    </div>
